81fb9a6665e34d4435829fabd711c313916d13e3
nicosio2 <nico.gruendel@web.de>
2015-05-16 23:56:13 +0200

    Konstruktor hinzugefügt

--- Minesweeper Game-Logic/src/main/java/org/pixelgaffer/turnierserver/minesweeper/logic/MinesweeperLogic.java
+++ Minesweeper Game-Logic/src/main/java/org/pixelgaffer/turnierserver/minesweeper/logic/MinesweeperLogic.java
@@ -2,13 +2,20 @@ package org.pixelgaffer.turnierserver.minesweeper.logic;
 
 import org.pixelgaffer.turnierserver.gamelogic.AllBuilderAllSolverLogic;
 import org.pixelgaffer.turnierserver.gamelogic.interfaces.Ai;
+import org.pixelgaffer.turnierserver.gamelogic.messages.BuilderSolverResponse;
 import org.pixelgaffer.turnierserver.minesweeper.Cell;
 import org.pixelgaffer.turnierserver.minesweeper.Grid;
 import org.pixelgaffer.turnierserver.minesweeper.MinesweeperBuilderResponse;
 import org.pixelgaffer.turnierserver.minesweeper.MinesweeperSolverResponse;
 
-public class MinesweeperLogic extends AllBuilderAllSolverLogic<MinesweeperObject, Grid, MinesweeperBuilderResponse, MinesweeperSolverResponse> {
+import com.google.gson.reflect.TypeToken;
 
+public class MinesweeperLogic extends AllBuilderAllSolverLogic<MinesweeperObject, Grid, MinesweeperBuilderResponse, MinesweeperSolverResponse> {
+	
+	public MinesweeperLogic() {
+		super(new TypeToken<BuilderSolverResponse<MinesweeperBuilderResponse, MinesweeperSolverResponse>>() {});
+	}
+	
 	@Override
 	public void failed(boolean building, Ai ai) {
 		if(building) {
